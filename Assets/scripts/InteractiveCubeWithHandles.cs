13112077f80ff9333b27e5fb28fcf9bbe6e788bc
aHauntingPotato <sarah.c.loup@gmail.com>
2022-08-22 21:13:53 +0200

    moveable crate with handles

---
+++ Assets/scripts/InteractiveCubeWithHandles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class InteractiveCubeWithHandles : MonoBehaviourPun
+{
+
+    public GameObject leftHandle;
+    public GameObject rightHandle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = (leftHandle.transform.position + rightHandle.transform.position) / 2;
+    }
+}
